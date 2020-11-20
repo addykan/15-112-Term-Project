85c825d934e5f7001fed2439c1ef365820996d32
Adhvik Kanagala <52569612+addykan@users.noreply.github.com>
2020-11-19 23:35:02 -0500

    Added extra modules and modified web browser functionality

--- OAuth2.py
+++ OAuth2.py
@@ -1,2 +1,5 @@
-import requests
+import requests, requests_oauthlib, webbrowser
 
+# https://stackoverflow.com/questions/47118598/python-how-to-open-default
+# -browser-using-webbrowser-module
+webbrowser.get('windows-default').open('https://youtube.com')
